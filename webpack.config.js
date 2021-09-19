ddc4a2c02c6f44d98c04a265c15291a8cec41c7e
Kyle Fang <zhigang1992@gmail.com>
2021-09-19 21:21:19 +0800

    fix: build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const mode = process.env.NODE_ENV || "production";
 
 module.exports = {
   output: {
-    filename: `worker.${mode}.js`,
+    filename: `worker.js`,
     path: path.join(__dirname, "dist"),
   },
   target: "webworker",
