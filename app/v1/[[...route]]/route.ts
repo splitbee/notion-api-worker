73569ec1ddb8dfb43d3e95543a9144b2940c14f0
Tobias Lins <me@tobi.sh>
2024-07-30 09:35:03 +0200

    Update route.ts

--- app/v1/[[...route]]/route.ts
+++ app/v1/[[...route]]/route.ts
@@ -1,18 +1,17 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 
-import { searchRoute } from '@/app/routes/search'
-import { tableRoute } from '@/app/routes/table'
-import { userRoute } from '@/app/routes/user'
-import { pageRoute } from '@/app/routes/page'
+import { pageRoute } from "../../routes/page";
+import { tableRoute } from "../../routes/table";
+import { userRoute } from "../../routes/user";
+import { searchRoute } from "../../routes/search";
 
-const app = new Hono().basePath('/v1')
+const app = new Hono().basePath("/v1");
 
 app.get("/page/:pageId", pageRoute);
 app.get("/table/:pageId", tableRoute);
 app.get("/user/:userId", userRoute);
 app.get("/search", searchRoute);
 
-
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
